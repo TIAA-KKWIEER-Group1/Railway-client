3054482c433a33855f0e0a4b7220da452ceadd47
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-05-05 21:52:06 +0530

    feat: Made the navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,70 @@
+import { useRef, useState } from 'react';
+import { MdOutlineClose } from 'react-icons/md';
+import { RxHamburgerMenu } from 'react-icons/rx';
 import { Link } from 'react-router-dom';
+import useOutsideAlerter from '../../hooks/useOutsideAlerter';
 import styles from './Navbar.module.css';
 
 function Navbar() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const handleCloseNavbar = () => {
+    setIsNavOpen(false);
+  };
+
+  // Navbar
+  const navigationRef = useRef(null);
+  useOutsideAlerter(navigationRef, handleCloseNavbar);
+
+  const toggleNav = () => {
+    setIsNavOpen((state) => !state);
+  };
+
   return (
-    <header className={styles.Navbar}>
-      <h1>
-        <Link to="/">Railway</Link>
-      </h1>
-      <ul className={styles.links}>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/user/login">User Login</Link>
-        </li>
-        <li>
-          <Link to="/user/register">User Register</Link>
-        </li>
-        <li>
-          <Link to="/admin/login">Admin Login</Link>
-        </li>
-      </ul>
+    <header className={styles.Header}>
+      <div className={`container ${styles.navWrapper}`}>
+        <Link to="/" className={styles.heading}>
+          Railway Reservation
+        </Link>
+        <button
+          type="button"
+          onClick={toggleNav}
+          className={styles.mobileNavToggle}
+          aria-controls="primary-navigation"
+        >
+          {!isNavOpen ? <RxHamburgerMenu /> : <MdOutlineClose />}
+          <span className="visually-hidden">Menu</span>
+        </button>
+
+        {/* Div is used to provide backdrop shadow */}
+        <div
+          className={`${styles.backdrop} ${
+            isNavOpen ? styles.backdropOpen : ''
+          }`}
+        ></div>
+
+        <nav
+          ref={navigationRef}
+          className={`${styles.primaryNavigation} ${
+            isNavOpen ? styles.primaryNavigationOpen : ''
+          }`}
+        >
+          <ul className={styles.navList} id="primary-navigation">
+            <li className={styles.navItem}>
+              <Link to="/">Home</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link to="/user/login">User Login</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link to="/user/register">User Register</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link to="/admin/login">Admin Login</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
---
+++ src/hooks/useOutsideAlerter.js
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+
+const useOutsideAlerter = (wrapperRef, actionCallback) => {
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.target) return;
+
+      const target = event.target;
+      if (wrapperRef.current && !wrapperRef.current.contains(target)) {
+        actionCallback();
+      }
+    };
+
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [wrapperRef, actionCallback]);
+};
+
+export default useOutsideAlerter;
