5095717a8b5f350462bb4daaade9671dab6a6feb
mansishardul <mansishardul@gmail.com>
2023-05-05 23:55:30 +0530

    admin panel

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,5 +1,15 @@
+import styles from './Admin.module.css';
 function Admin() {
-  return <div>Admin</div>;
+  return <div className={styles.wrapper}>
+    <header>Upload EXCEL OR CSV only</header>
+    <form action="#">
+      <input className={styles.file - input} type="file" name="file" hidden />
+      <i className={styles.fas fa-cloud-upload-alt ></i>
+    <p>Browse File to Upload</p>
+    <button id="upload_btn">Click to Upload Excel</button>
+  </form><section className={styles.progress-area}></section><section className={styles.uploaded - area}></section>
+ 
+</div>;
 }
 
 export default Admin;
