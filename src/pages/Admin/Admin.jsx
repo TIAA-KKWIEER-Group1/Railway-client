3a61bd217ed3e0296e0194b7959ce797c4380f6a
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-05-06 10:54:50 +0530

    fix: Admin bug fixes

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,15 +1,29 @@
+import { BiCloudUpload } from 'react-icons/bi';
 import styles from './Admin.module.css';
+
 function Admin() {
-  return <div className={styles.wrapper}>
-    <header>Upload EXCEL OR CSV only</header>
-    <form action="#">
-      <input className={styles.file - input} type="file" name="file" hidden />
-      <i className={styles.fas fa-cloud-upload-alt ></i>
-    <p>Browse File to Upload</p>
-    <button id="upload_btn">Click to Upload Excel</button>
-  </form><section className={styles.progress-area}></section><section className={styles.uploaded - area}></section>
- 
-</div>;
+  return (
+    <div className={styles.Admin}>
+      <div className={styles.wrapper}>
+        <header>Upload File</header>
+        <form>
+          <div className={styles.container}>
+            <input
+              className={styles.fileInput}
+              type="file"
+              name="file"
+              hidden
+            />
+            <BiCloudUpload />
+            <div>Click to Upload Excel</div>
+          </div>
+          <button>Upload</button>
+        </form>
+        <section className={styles.progressArea}></section>
+        <section className={styles.uploadedArea}></section>
+      </div>
+    </div>
+  );
 }
 
 export default Admin;
