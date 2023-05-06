525832221f12aba45eb88f16c8551acd5862bf2e
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-05-06 11:38:45 +0530

    fix: Fixed admin upload

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,23 +1,65 @@
+import axios from 'axios';
+import { useRef, useState } from 'react';
 import { BiCloudUpload } from 'react-icons/bi';
 import styles from './Admin.module.css';
 
 function Admin() {
+  const [file, setFile] = useState(null);
+  const fileInput = useRef();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    setIsLoading(true);
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const url =
+      import.meta.env.VITE_REACT_APP_BASE_API_URL + '/api/admin/csvData';
+
+    axios
+      .post(url, formData)
+      .then((response) => {
+        console.log(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+  };
+
+  const handleUploadClick = () => {
+    if (!fileInput.current) return;
+    fileInput.current.click();
+  };
+
   return (
     <div className={styles.Admin}>
       <div className={styles.wrapper}>
         <header>Upload File</header>
-        <form>
-          <div className={styles.container}>
+        <form onSubmit={handleFormSubmit}>
+          <div className={styles.container} onClick={handleUploadClick}>
             <input
               className={styles.fileInput}
+              ref={fileInput}
               type="file"
               name="file"
+              onChange={handleFileChange}
               hidden
             />
             <BiCloudUpload />
             <div>Click to Upload Excel</div>
+            {file ? <p className={styles.fileName}>{file.name}</p> : null}
           </div>
-          <button>Upload</button>
+          <button disabled={!file || isLoading}>Upload</button>
         </form>
         <section className={styles.progressArea}></section>
         <section className={styles.uploadedArea}></section>
