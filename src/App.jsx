4b577b3b5563d89f22b00a6dee1ad5a353b71b0f
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-05-06 12:12:42 +0530

    feat: Implemented search box of the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Toaster } from 'react-hot-toast';
 import Navbar from './components/Navbar/Navbar';
 import Router from './router/Router';
 
 function App() {
   return (
     <>
+      <Toaster
+        position="top-right"
+        containerStyle={{
+          top: 80,
+          zIndex: 100000,
+        }}
+      />
+
       <Navbar />
       <Router />
     </>
