c26eea6f7c15723e2b3e3568b76e4692c7d3f735
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-05-06 11:37:40 +0530

    fix: train search fix

--- src/pages/TrainSearch/TrainSearch.jsx
+++ src/pages/TrainSearch/TrainSearch.jsx
@@ -1,8 +1,8 @@
-import style from './TrainSearch.module.css';
+import styles from './TrainSearch.module.css';
 function TrainSearch() {
   return (
-    <div>
-      <div className={style.searchContainer}>
+    <div className={styles.TrainSearch}>
+      <div className={styles.searchContainer}>
         <form action="#">
           <input type="text" placeholder="Destination..." name="destination" />
           <input type="text" placeholder="Source..." name="source" />
@@ -14,47 +14,47 @@ function TrainSearch() {
       </div>
 
       <main>
-        <div className={style.container}>
-          <div className={style.cardHorizontal}>
-            <div className={style.navbar}>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Train Name:</span>
-                <span className={style.navbarValue}>Express 2</span>
+        <div className={styles.container}>
+          <div className={styles.cardHorizontal}>
+            <div className={styles.navbar}>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Train Name:</span>
+                <span className={styles.navbarValue}>Express 2</span>
               </div>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Date:</span>
-                <span className={style.navbarValue}>5th May, 2023</span>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Date:</span>
+                <span className={styles.navbarValue}>5th May, 2023</span>
               </div>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Source:</span>
-                <span className={style.navbarValue}>New Delhi</span>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Source:</span>
+                <span className={styles.navbarValue}>New Delhi</span>
               </div>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Destination:</span>
-                <span className={style.navbarValue}>Mumbai</span>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Destination:</span>
+                <span className={styles.navbarValue}>Mumbai</span>
               </div>
             </div>
-            <div className={style.cardDetails}>
-              <div className={style.cardInfo}>
-                <h2 className={style.cardTitle}>Train Name</h2>
-                <p className={style.cardText}>Date: 10/05/2023</p>
-                <p className={style.cardText}>Arrival Time: 00:00:00 AM</p>
-                <p className={style.cardText}>Departure Time: 00:00:00 AM</p>
+            <div className={styles.cardDetails}>
+              <div className={styles.cardInfo}>
+                <h2 className={styles.cardTitle}>Train Name</h2>
+                <p className={styles.cardText}>Date: 10/05/2023</p>
+                <p className={styles.cardText}>Arrival Time: 00:00:00 AM</p>
+                <p className={styles.cardText}>Departure Time: 00:00:00 AM</p>
               </div>
-              <div className={style.cardButtons}>
-                <button className={style.btn} />
+              <div className={styles.cardButtons}>
+                <button className={styles.btn} />
                 Sleeper
-                <button className={style.btn} />
+                <button className={styles.btn} />
                 AC First Class
               </div>
-              <div className={style.btnGroup}>
-                <a href="#" className={style.bookNowBtn}>
+              <div className={styles.btnGroup}>
+                <a href="#" className={styles.bookNowBtn}>
                   Book Now
                 </a>
                 <a
                   href="route.html"
                   id="btn-1"
-                  className={style.viewDetailsBtn}
+                  className={styles.viewDetailsBtn}
                 >
                   View more
                 </a>
@@ -62,46 +62,46 @@ function TrainSearch() {
             </div>
           </div>
 
-          <div className={style.cardHorizontal}>
-            <div className={style.navbar}>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Train Name:</span>
-                <span className={style.navbarValue}>Express 2</span>
+          <div className={styles.cardHorizontal}>
+            <div className={styles.navbar}>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Train Name:</span>
+                <span className={styles.navbarValue}>Express 2</span>
               </div>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Date:</span>
-                <span className={style.navbarValue}>5th May, 2023</span>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Date:</span>
+                <span className={styles.navbarValue}>5th May, 2023</span>
               </div>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Source:</span>
-                <span className={style.navbarValue}>New Delhi</span>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Source:</span>
+                <span className={styles.navbarValue}>New Delhi</span>
               </div>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Destination:</span>
-                <span className={style.navbarValue}>Mumbai</span>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Destination:</span>
+                <span className={styles.navbarValue}>Mumbai</span>
               </div>
             </div>
-            <div className={style.cardDetails}>
-              <div className={style.cardInfo}>
-                <h2 className={style.cardTitle}>Train Name</h2>
-                <p className={style.cardText}>Date: 10/05/2023</p>
-                <p className={style.cardText}>Arrival Time: 00:00:00 AM</p>
-                <p className={style.cardText}>Departure Time: 00:00:00 AM</p>
+            <div className={styles.cardDetails}>
+              <div className={styles.cardInfo}>
+                <h2 className={styles.cardTitle}>Train Name</h2>
+                <p className={styles.cardText}>Date: 10/05/2023</p>
+                <p className={styles.cardText}>Arrival Time: 00:00:00 AM</p>
+                <p className={styles.cardText}>Departure Time: 00:00:00 AM</p>
               </div>
-              <div className={style.cardButtons}>
-                <button className={style.btn} />
+              <div className={styles.cardButtons}>
+                <button className={styles.btn} />
                 Sleeper
-                <button className={style.btn} />
+                <button className={styles.btn} />
                 AC First Class
               </div>
-              <div className={style.btnGroup}>
-                <a href="#" className={style.bookNowBtn}>
+              <div className={styles.btnGroup}>
+                <a href="#" className={styles.bookNowBtn}>
                   Book Now
                 </a>
                 <a
                   href="route.html"
                   id="btn-1"
-                  className={style.viewDetailsBtn}
+                  className={styles.viewDetailsBtn}
                 >
                   View more
                 </a>
@@ -109,46 +109,46 @@ function TrainSearch() {
             </div>
           </div>
 
-          <div className={style.cardHorizontal}>
-            <div className={style.navbar}>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Train Name:</span>
-                <span className={style.navbarValue}>Express 2</span>
+          <div className={styles.cardHorizontal}>
+            <div className={styles.navbar}>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Train Name:</span>
+                <span className={styles.navbarValue}>Express 2</span>
               </div>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Date:</span>
-                <span className={style.navbarValue}>5th May, 2023</span>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Date:</span>
+                <span className={styles.navbarValue}>5th May, 2023</span>
               </div>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Source:</span>
-                <span className={style.navbarValue}>New Delhi</span>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Source:</span>
+                <span className={styles.navbarValue}>New Delhi</span>
               </div>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Destination:</span>
-                <span className={style.navbarValue}>Mumbai</span>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Destination:</span>
+                <span className={styles.navbarValue}>Mumbai</span>
               </div>
             </div>
-            <div className={style.cardDetails}>
-              <div className={style.cardInfo}>
-                <h2 className={style.cardTitle}>Train Name</h2>
-                <p className={style.cardText}>Date: 10/05/2023</p>
-                <p className={style.cardText}>Arrival Time: 00:00:00 AM</p>
-                <p className={style.cardText}>Departure Time: 00:00:00 AM</p>
+            <div className={styles.cardDetails}>
+              <div className={styles.cardInfo}>
+                <h2 className={styles.cardTitle}>Train Name</h2>
+                <p className={styles.cardText}>Date: 10/05/2023</p>
+                <p className={styles.cardText}>Arrival Time: 00:00:00 AM</p>
+                <p className={styles.cardText}>Departure Time: 00:00:00 AM</p>
               </div>
-              <div className={style.cardButtons}>
-                <button className={style.btn} />
+              <div className={styles.cardButtons}>
+                <button className={styles.btn} />
                 Sleeper
-                <button className={style.btn} />
+                <button className={styles.btn} />
                 AC First Class
               </div>
-              <div className={style.btnGroup}>
-                <a href="#" className={style.bookNowBtn}>
+              <div className={styles.btnGroup}>
+                <a href="#" className={styles.bookNowBtn}>
                   Book Now
                 </a>
                 <a
                   href="route.html"
                   id="btn-1"
-                  className={style.viewDetailsBtn}
+                  className={styles.viewDetailsBtn}
                 >
                   View more
                 </a>
@@ -156,46 +156,46 @@ function TrainSearch() {
             </div>
           </div>
 
-          <div className={style.cardHorizontal}>
-            <div className={style.navbar}>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Train Name:</span>
-                <span className={style.navbarValue}>Express 2</span>
+          <div className={styles.cardHorizontal}>
+            <div className={styles.navbar}>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Train Name:</span>
+                <span className={styles.navbarValue}>Express 2</span>
               </div>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Date:</span>
-                <span className={style.navbarValue}>5th May, 2023</span>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Date:</span>
+                <span className={styles.navbarValue}>5th May, 2023</span>
               </div>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Source:</span>
-                <span className={style.navbarValue}>New Delhi</span>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Source:</span>
+                <span className={styles.navbarValue}>New Delhi</span>
               </div>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Destination:</span>
-                <span className={style.navbarValue}>Mumbai</span>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Destination:</span>
+                <span className={styles.navbarValue}>Mumbai</span>
               </div>
             </div>
-            <div className={style.cardDetails}>
-              <div className={style.cardInfo}>
-                <h2 className={style.cardTitle}>Train Name</h2>
-                <p className={style.cardText}>Date: 10/05/2023</p>
-                <p className={style.cardText}>Arrival Time: 00:00:00 AM</p>
-                <p className={style.cardText}>Departure Time: 00:00:00 AM</p>
+            <div className={styles.cardDetails}>
+              <div className={styles.cardInfo}>
+                <h2 className={styles.cardTitle}>Train Name</h2>
+                <p className={styles.cardText}>Date: 10/05/2023</p>
+                <p className={styles.cardText}>Arrival Time: 00:00:00 AM</p>
+                <p className={styles.cardText}>Departure Time: 00:00:00 AM</p>
               </div>
-              <div className={style.cardButtons}>
-                <button className={style.btn} />
+              <div className={styles.cardButtons}>
+                <button className={styles.btn} />
                 Sleeper
-                <button className={style.btn} />
+                <button className={styles.btn} />
                 AC First Class
               </div>
-              <div className={style.btnGroup}>
-                <a href="#" className={style.bookNowBtn}>
+              <div className={styles.btnGroup}>
+                <a href="#" className={styles.bookNowBtn}>
                   Book Now
                 </a>
                 <a
                   href="route.html"
                   id="btn-1"
-                  className={style.viewDetailsBtn}
+                  className={styles.viewDetailsBtn}
                 >
                   View more
                 </a>
@@ -203,46 +203,46 @@ function TrainSearch() {
             </div>
           </div>
 
-          <div className={style.cardHorizontal}>
-            <div className={style.navbar}>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Train Name:</span>
-                <span className={style.navbarValue}>Express 2</span>
+          <div className={styles.cardHorizontal}>
+            <div className={styles.navbar}>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Train Name:</span>
+                <span className={styles.navbarValue}>Express 2</span>
               </div>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Date:</span>
-                <span className={style.navbarValue}>5th May, 2023</span>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Date:</span>
+                <span className={styles.navbarValue}>5th May, 2023</span>
               </div>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Source:</span>
-                <span className={style.navbarValue}>New Delhi</span>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Source:</span>
+                <span className={styles.navbarValue}>New Delhi</span>
               </div>
-              <div className={style.navbarItem}>
-                <span className={style.navbarLabel}>Destination:</span>
-                <span className={style.navbarValue}>Mumbai</span>
+              <div className={styles.navbarItem}>
+                <span className={styles.navbarLabel}>Destination:</span>
+                <span className={styles.navbarValue}>Mumbai</span>
               </div>
             </div>
-            <div className={style.cardDetails}>
-              <div className={style.cardInfo}>
-                <h2 className={style.cardTitle}>Train Name</h2>
-                <p className={style.cardText}>Date: 10/05/2023</p>
-                <p className={style.cardText}>Arrival Time: 00:00:00 AM</p>
-                <p className={style.cardText}>Departure Time: 00:00:00 AM</p>
+            <div className={styles.cardDetails}>
+              <div className={styles.cardInfo}>
+                <h2 className={styles.cardTitle}>Train Name</h2>
+                <p className={styles.cardText}>Date: 10/05/2023</p>
+                <p className={styles.cardText}>Arrival Time: 00:00:00 AM</p>
+                <p className={styles.cardText}>Departure Time: 00:00:00 AM</p>
               </div>
-              <div className={style.cardButtons}>
-                <button className={style.btn} />
+              <div className={styles.cardButtons}>
+                <button className={styles.btn} />
                 Sleeper
-                <button className={style.btn} />
+                <button className={styles.btn} />
                 AC First Class
               </div>
-              <div className={style.btnGroup}>
-                <a href="#" className={style.bookNowBtn}>
+              <div className={styles.btnGroup}>
+                <a href="#" className={styles.bookNowBtn}>
                   Book Now
                 </a>
                 <a
                   href="route.html"
                   id="btn-1"
-                  className={style.viewDetailsBtn}
+                  className={styles.viewDetailsBtn}
                 >
                   View more
                 </a>
